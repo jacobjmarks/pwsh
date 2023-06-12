a352ef6b7a42b0d766defac5158d4571083e0a35
Jacob Marks <jacobjmarks@outlook.com>
2023-06-12 21:31:35 +1000

    Inline profile

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Windows PowerShell Bootstrapper
 
+``` pwsh
+irm https://raw.githubusercontent.com/jacobjmarks/pwsh/main/bootstrap.ps1 | iex
+```
+
 ![Preview](preview.png)
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,46 +1,52 @@
 $ErrorActionPreference = "Stop"
 
-Push-Location (Split-Path $MyInvocation.MyCommand.Path)
+# PowerShell https://github.com/PowerShell/PowerShell
+Write-Host "> Installing PowerShell ..."
+winget install -e --id Microsoft.PowerShell
 
-try {
-    # PowerShell https://github.com/PowerShell/PowerShell
-    Write-Host "> Installing PowerShell ..."
-    winget install -e --id Microsoft.PowerShell
+# Git https://git-scm.com/download/win
+Write-Host "> Installing Git ..."
+winget install -e --id Git.Git
 
-    # Git https://git-scm.com/download/win
-    Write-Host "> Installing Git ..."
-    winget install -e --id Git.Git
+# Oh My Posh (via Microsoft Store) https://github.com/jandedobbeleer/oh-my-posh
+Write-Host "> Installing Oh My Posh ..."
+winget install -e --id XP8K0HKJFRXGCK
 
-    # Oh My Posh (via Microsoft Store) https://github.com/jandedobbeleer/oh-my-posh
-    Write-Host "> Installing Oh My Posh ..."
-    winget install -e --id XP8K0HKJFRXGCK
+# Oh My Posh - Theme
+Write-Host "> Downloading Oh My Posh theme ..."
+Invoke-WebRequest  | Out-File "$env:POSH_THEMES_PATH\paradox.omp.json"
 
-    # Oh My Posh - Theme
-    Write-Host "> Downloading Oh My Posh theme ..."
-    Invoke-WebRequest  | Out-File "$env:POSH_THEMES_PATH\paradox.omp.json"
+# Terminal-Icons https://github.com/devblackops/Terminal-Icons
+Write-Host "> Installing Terminal-Icons ..."
+Install-Module Terminal-Icons -Repository PSGallery
 
-    # Terminal-Icons https://github.com/devblackops/Terminal-Icons
-    Write-Host "> Installing Terminal-Icons ..."
-    Install-Module Terminal-Icons -Repository PSGallery
+# posh-git https://github.com/dahlbyk/posh-git
+Write-Host "> Installing posh-git ..."
+Install-Module posh-git -Force
 
-    # posh-git https://github.com/dahlbyk/posh-git
-    Write-Host "> Installing posh-git ..."
-    Install-Module posh-git -Force
+# # PSReadLine https://github.com/PowerShell/PSReadLine
+# Write-Host "> Installing PSReadLine ..."
+# Install-Module PSReadLine -Scope CurrentUser -AllowPrerelease -Force
 
-    # # PSReadLine https://github.com/PowerShell/PSReadLine
-    # Write-Host "> Installing PSReadLine ..."
-    # Install-Module PSReadLine -Scope CurrentUser -AllowPrerelease -Force
+# z https://github.com/badmotorfinger/z
+Write-Host "> Installing z ..."
+Install-Module z -Force
 
-    # z https://github.com/badmotorfinger/z
-    Write-Host "> Installing z ..."
-    Install-Module z -Force
+# Configure PowerShell profile
+Write-Host "> Configuring PowerShell profile ..."
+$ProfileScript = {
+    Import-Module posh-git
+    Import-Module Terminal-Icons
 
-    # Configure PowerShell profile
-    Write-Host "> Configuring PowerShell profile ..."
-    Get-Content .\profile.ps1 | Out-File $PROFILE
+    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\paradox.omp.json" | Invoke-Expression
 
-    Write-Host "> Done!"
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionViewStyle ListView
+
+    Set-Alias grep findstr
+    Set-Alias which gcm
 }
-finally {
-    Pop-Location
-}
+
+$ProfileScript.ToString().Trim() -replace "        ", "" | Out-File $PROFILE
+
+Write-Host "> Done!"
