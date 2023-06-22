1e5818b48ba4a680770389e47db0a6cebb873927
Jacob Marks <jacobjmarks@outlook.com>
2023-06-22 16:42:00 +1000

    Implement Terminal settings updater
    Fix out-file encoding issues

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -41,6 +41,98 @@ function RefreshPath {
     ) -match '.' -join ';'
 }
 
+function Get-FontFamilyName {
+    param (
+        [Parameter(Mandatory)]
+        [string] $File
+    )
+
+    Add-Type -AssemblyName System.Drawing
+    $FontCollection = [System.Drawing.Text.PrivateFontCollection]::new()
+    try {
+        $FontCollection.AddFontFile($File)
+        $FontCollection.Families.Name
+    }
+    finally {
+        $FontCollection.Dispose()
+    }
+}
+
+function Update-TerminalSettings {
+    Write-Host "> Checking Windows Terminal settings ..."
+
+    $TerminalSettingsFile = [System.IO.FileInfo](Join-Path $env:LOCALAPPDATA "Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json")
+    $TerminalSettings = Get-Content $TerminalSettingsFile.FullName | ConvertFrom-Json
+
+    $PowerShellCoreProfile = $TerminalSettings.profiles.list | Where-Object { $_.source -eq "Windows.Terminal.PowershellCore" }
+
+    $Manifest = @(
+        @{
+            SettingPath  = "defaultProfile"
+            CurrentValue = $TerminalSettings.defaultProfile
+            DesiredValue = $PowerShellCoreProfile.guid
+        }
+        @{
+            SettingPath  = "profiles.defaults.colorScheme"
+            CurrentValue = $TerminalSettings.profiles.defaults.colorScheme
+            DesiredValue = "One Half Dark"
+        }
+        @{
+            SettingPath  = "profiles.defaults.font.face"
+            CurrentValue = $TerminalSettings.profiles.defaults.font.face
+            DesiredValue = "$NerdFontFamilyName"
+        }
+        @{
+            SettingPath  = "profiles.defaults.opacity"
+            CurrentValue = $TerminalSettings.profiles.defaults.opacity
+            DesiredValue = 75
+        }
+        @{
+            SettingPath  = "profiles.defaults.useAcrylic"
+            CurrentValue = $TerminalSettings.profiles.defaults.useAcrylic
+            DesiredValue = $true
+        }
+        @{
+            SettingPath  = "useAcrylicInTabRow"
+            CurrentValue = $TerminalSettings.useAcrylicInTabRow
+            DesiredValue = $true
+        }
+    )
+
+    $DiscrepantSettings = $Manifest | Where-Object { $_.CurrentValue -ne $_.DesiredValue }
+    if (-Not $DiscrepantSettings) {
+        return
+    }
+
+    foreach ($Entry in $DiscrepantSettings) {
+        Write-Host "    $($Entry.SettingPath) = $(if($Entry.CurrentValue){$Entry.CurrentValue}else{"NULL"}) -> $($Entry.DesiredValue)"
+    }
+
+    $ShouldProceed = Read-Host "> Update Windows Terminal settings as above? (recommended) [yN]"
+    if ($ShouldProceed.Trim().ToLower() -ne 'y') {
+        return
+    }
+
+    foreach ($Entry in $DiscrepantSettings) {
+        $Segments = $Entry.SettingPath -split "\."
+
+        # hydrate
+        $Ptr = $TerminalSettings
+        foreach ($Segment in ($Segments | Select-Object -SkipLast 1)) {
+            if (-Not $Ptr."$Segment") {
+                $Ptr | Add-Member -NotePropertyName $Segment -NotePropertyValue ([PSCustomObject]@{})
+            }
+            $Ptr = $Ptr."$Segment"
+        }
+
+        # set desired value
+        $Ptr | Add-Member -NotePropertyName ($Segments | Select-Object -Last 1) -NotePropertyValue $Entry.DesiredValue
+    }
+
+    $TerminalSettings | ConvertTo-Json -Depth 100 | Out-File $TerminalSettingsFile -Encoding utf8
+    Write-Host "> Updated: $TerminalSettingsFile"
+}
+
 $Steps = @(
     @{
         Descriptor  = "Installing Windows Terminal"
@@ -124,7 +216,7 @@ $Steps = @(
                 }
             }
 
-            $ProfileScript.Trim() -replace "                ", "" | Out-File $PwshProfile
+            $ProfileScript.Trim() -replace "                ", "" | Out-File $PwshProfile -Encoding utf8
             Write-Host "> Updated: $PwshProfile"
         }
     }
@@ -167,6 +259,7 @@ $Steps = @(
             if ($FontFiles.Count -eq 0) {
                 throw "No files found within asset '$TargetAssetName' matching the filter '$FontFileFilter'"
             }
+            $script:NerdFontFamilyName = Get-FontFamilyName ($FontFiles | Select-Object -First 1).Path
             $FontsFolder = $ShellApplication.Namespace(0x14)
             $FontsFolder.CopyHere($FontFiles)
         }
@@ -187,5 +280,6 @@ if ($NonTerminatingErrorCount -gt 0) {
             + "It is recommended you resolve these errors and re-run the script.")
 }
 else {
-    Write-Host "`n> Done!"
+    Update-TerminalSettings
+    Write-Host "> Bootstrapping Complete!"
 }
